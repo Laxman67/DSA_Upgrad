0989a0ff7c7193d4fb32fc2ce51c1fea3b3cd7bf
laxman67 <laxmanhenricks94@gmail.com>
2024-06-22 09:27:54 +0530

    DLL [push,pop,shift,unshift]

--- 05_DoublyLinkedList/0_DLL.js
+++ 05_DoublyLinkedList/0_DLL.js
@@ -28,9 +28,65 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.tail;
+
+    if (this.length == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
+  unshift(value) {
+    let newNode = new Node(value);
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return newNode;
+  }
+
+  shift() {
+    if (this.length === 0) return 'stack Undeflow';
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+  print() {
+    let temp = this.head;
+    let i = 0;
+
+    // run till this.head is not null
+    while (temp) {
+      console.log(`${i} :=> ${temp.value}`);
+      temp = temp.next;
+      i++;
+    }
+  }
 }
 
-let dll = new DoublyLinkedList(90);
-dll.push(76);
-dll.push(5);
-console.dir(dll);
+let dll = new DoublyLinkedList(1);
+dll.push(2);
+dll.push(9);
+console.log(dll.shift());
+console.log(dll);
