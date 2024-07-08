8386f96422917737654efdaf0b1e9ef8a5313031
laxman67 <laxmanhenricks94@gmail.com>
2024-07-08 23:17:24 +0530

    DoublyLinkedList Completed

---
+++ 05_DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,147 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    let newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  //   Push Operation - Adding node to the end of the LinkedList
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  // Pop Operation - Popping out the last node from the LinkedList
+  pop() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+  // Adding a new  Node to the beginning of the LinkedList
+  unshift(value) {
+    let newNode = new Node(value);
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // Removing a node from then beginning of the linkedList
+  shift() {
+    if (this.length == 0) return undefined;
+    let temp = this.head;
+    if (this.length == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  // Get a Node from the particular index position
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = this.tail;
+
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
+      return temp;
+    }
+    return temp;
+  }
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    let newNode = new Node(value);
+    const before = this.get(index - 1);
+    const after = before.next;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+    this.length++;
+    return true;
+
+    return true;
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return false;
+
+    let temp = this.get(index);
+
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+
+    this.length--;
+    return temp;
+  }
+}
+
+let dll = new DoublyLinkedList(0);
+dll.push(1);
+dll.push(2);
+
+console.log(dll.remove(1));
+
+console.log(dll);
